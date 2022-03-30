05392aad54a0c1ea27b5cf103ab00a9659f7144a
Pratik Thorat <pratik.thorat7@gmail.com>
2022-03-30 18:03:43 +0530

    fix: undo audio onload

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
 
 })(jQuery);
 
-$(document).ready(function() {
+$(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
